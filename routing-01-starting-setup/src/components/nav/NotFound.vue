d1f83e1fce7d9ae4a931e5ddbdcf2949cf31aaa4
iiwal <iiwally26@gmail.com>
2025-07-24 13:59:52 -0500

    coding sucks

---
+++ routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>Not Found! Click here to check out teams:
+  <router-link to="/teams">Teams</router-link>
+  </h2>
+</template>
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -7,14 +7,39 @@ import App from './App.vue';
 import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
+import NotFound from "./components/nav/NotFound.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/teams', component: TeamsList },
+        { path: '/', redirect: '/teams'},
+        {
+            name: 'teams',
+            path: '/teams',
+            component: TeamsList,
+            children: [
+                { name: 'team-members', path: '/teams/:teamId', component: TeamMembers, props: true },
+            ]
+        },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers },
-    ]
+        { path: '/:notFound(.*)', component: NotFound }
+    ],
+    linkActiveClass: 'active',
+    scrollBehavior (to, from, savedPosition) {
+        console.log(to, from, savedPosition);
+        if(savedPosition) {
+            return savedPosition;
+        }
+        return { left: 0, top: 0};
+    }
+});
+
+router.beforeEach(function(to, from, next) {
+    if (to.name === 'team-members'){
+        next();
+    }else{
+        next({name: 'team-members', params: {teamId: 't2'}});
+    }
 })
 const app = createApp(App)
 
